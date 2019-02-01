290b1107b4c037ac4d5b3aaea5e1c6fe7e79c73a
Felipe Tavares <felipe@tavares@outlook.com>
2019-01-31 22:25:20 -0200

    Incluindo classe para ADO NET

---
+++ AgendaADONET/DAO/DAOUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaADONET.DAO
+{
+    class DAOUtils
+    {
+        public static DbConnection GetConexao()
+        {
+            DbConnection conexao = new SqlConnection("Server=.\SQLEXPRESS;Database=TreinaWebCSharpIntermediario;User Id=sa;Password=865522;");
+        }
+    }
+}

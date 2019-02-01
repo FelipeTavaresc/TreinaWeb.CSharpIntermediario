a0706a6f99f279338cba1358dedcc40ffc07f987
Felipe Tavares <felipe@tavares@outlook.com>
2019-01-31 22:27:22 -0200

    Incluindo classe ADO NET

--- AgendaADONET/DAO/DAOUtils.cs
+++ AgendaADONET/DAO/DAOUtils.cs
@@ -12,7 +12,20 @@ namespace AgendaADONET.DAO
     {
         public static DbConnection GetConexao()
         {
-            DbConnection conexao = new SqlConnection("Server=.\SQLEXPRESS;Database=TreinaWebCSharpIntermediario;User Id=sa;Password=865522;");
+            DbConnection conexao = new SqlConnection(@"Server=.\SQLEXPRESS;Database=TreinaWebCSharpIntermediario;User Id=sa;Password=865522;");
+            conexao.Open();
+            return conexao;
+        }
+
+        public static DbCommand GetComando(DbConnection conexao)
+        {
+            DbCommand comando = conexao.CreateCommand();
+            return comando;
+        }
+
+        public static DbDataReader GetDataReader(DbCommand comando)
+        {
+            return comando.ExecuteReader();
         }
     }
 }

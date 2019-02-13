aed623a599bddbf132d0cb091110d7c112d10e1c
Felipe Tavares <felipe@tavares@outlook.com>
2019-02-12 22:18:21 -0200

    Conexão com o banco de dados via leitura de xml apSettings

--- AgendaADONET/DAO/DAOUtils.cs
+++ AgendaADONET/DAO/DAOUtils.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
@@ -13,8 +14,25 @@ namespace AgendaADONET.DAO
     {
         public static DbConnection GetConexao()
         {
-            //DbConnection conexao = new SqlConnection(@"Server=.\SQLEXPRESS;Database=TreinaWebCSharpIntermediario;User Id=sa;Password=865522;");
-            DbConnection conexao = new MySqlConnection(@"Server=localhost;Database=treinawebcsharpintermediario;Uid=root;Pwd=865522;");
+            string server = ConfigurationManager.AppSettings["server"].ToString();
+            string database = ConfigurationManager.AppSettings["database"].ToString();
+            string user = ConfigurationManager.AppSettings["user"].ToString();
+            string password = ConfigurationManager.AppSettings["password"].ToString();
+
+            DbConnection conexao = null;
+
+            string connectionString = "";
+
+            if (ConfigurationManager.AppSettings["provider"].ToString().Equals("MSSQL"))
+            {
+                connectionString = @"Server=" + server + ";Database=" + database + ";User Id=" + user + ";Password=" + password + ";";
+                conexao = new SqlConnection(connectionString);
+            }
+            else
+            {
+                connectionString = @"Server=" + server + ";Database=" + database + ";Uid=" + user + ";Pwd=" + password + ";";
+                conexao = new MySqlConnection(connectionString);
+            }
             conexao.Open();
             return conexao;
         }
@@ -28,6 +46,20 @@ namespace AgendaADONET.DAO
         public static DbDataReader GetDataReader(DbCommand comando)
         {
             return comando.ExecuteReader();
+        }
+
+        public static DbParameter GetParametro(string nome, object valor)
+        {
+            DbParameter parametro = null;
+            if (ConfigurationManager.AppSettings["provider"].ToString().Equals("MSSQL"))
+            {
+                parametro = new SqlParameter(nome, valor);
+            }
+            else
+            {
+                parametro = new MySqlParameter(nome, valor);
+            }
+            return parametro;
         }
     }
 }
